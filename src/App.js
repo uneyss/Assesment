2659d214854a32a92172987eb56c85a4d6234059
uneyss <73793862+uneyss@users.noreply.github.com>
2023-02-02 20:08:05 +0300

    Update App.js
    Updated App.js
    Imported the tailwind css

--- src/App.js
+++ src/App.js
@@ -5,19 +5,26 @@ import PricingCard from "./components/PricingCard/PricingCard";
 export default function App() {
   return (
     <div className="App">
-
-        <h1>Firs Commit for stesssst</h1>
         <h1 className="text-3 font-bold underline">Hello worldss!</h1>
+        <h1 className="text-3 font-bold underline">Lets Get Start!</h1>
 
       <PricingCard
         title="Basic Plan"
         logo="basic-plan-logo.png"
         priceText="$10/month"
         features={[
-          { id: 1, title: "Feature 1", goodFeature: true },
-          { id: 2, title: "Feature 2", goodFeature: false },
-          { id: 3, title: "Feature 3", goodFeature: true },
-          { id: 4, title: "Feature 4", goodFeature: false }
+          { id: 1, 
+            title: "Feature 1", 
+            goodFeature: true },
+          { id: 2, 
+            title: "Feature 2", 
+            goodFeature: false },
+          { id: 3, 
+            title: "Feature 3", 
+            goodFeature: true },
+          { id: 4, 
+            title: "Feature 4", 
+            goodFeature: false }
         ]}
         buttonText="Subscribe"
       />
